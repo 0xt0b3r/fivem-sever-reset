6c9f9c5a79e5d6a095ee9ea15002af50c1f05545
Trey Woods <46387468+twoods10123@users.noreply.github.com>
2019-01-06 10:52:39 -0500

    Update server.lua

--- reset/server.lua
+++ reset/server.lua
@@ -1,12 +1,13 @@
 RegisterCommand('reset', function(source)
-	local _source = source
+    local _source = source
     local playername = GetPlayerName(_source)
 	if IsPlayerAceAllowed(source, 'w.reset') then
-	TriggerClientEvent('chatMessage', -1, '^1The server is being reset by ^5 ' .. playername .. '^1!')
-	TriggerClientEvent('delcar', -1)
+		TriggerClientEvent('chatMessage', -1, '^1The server is being reset by ^5 ' .. playername .. '^1!')
+		TriggerClientEvent('delcar', -1)
 	--^^ This is here to remove the player last vehicle and also here because if it was the last thing on the list it the below functions would TP the player to the spawnpoints which then will not delete the ped is in.
-	
-	 StopResource("fivem") -- These will remove all vehicles but the players last.
-     StartResource("fivem")
+		StopResource("fivem") -- These will remove all vehicles but the players last.
+     		StartResource("fivem")
+	 else
+	 	TriggerClientEvent('chatMessage', -1, "^1 You are not allowed to do this action!")
 	 end
 end, false)
