828f61a561ef6c1fe5b7ed432eefe8217790418a
Trey Woods <46387468+twoods10123@users.noreply.github.com>
2019-01-06 10:51:51 -0500

    Update client.lua

--- reset/client.lua
+++ reset/client.lua
@@ -1,11 +1,10 @@
 RegisterNetEvent('delcar') -- Event called when x player triggers the command set to check if player is in a car.
 AddEventHandler('delcar', function()
     local player = GetPlayerPed(-1)
-	local cars = GetVehiclePedIsIn(player, false)
-        if (GetPedInVehicleSeat(cars, -1) == player) then 
-				SetEntityAsMissionEntity(cars, true, true)
-            remCar(cars)
-		end
+	local cars = GetVehiclePedIsIn(player, true)
+		SetEntityAsMissionEntity(cars, true, true)
+            		remCar(cars)
+
 end )
 
 function remCar(entity) -- Function to remove the car.
